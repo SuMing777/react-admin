f7aaabcdd5e3f46c9303a2deaed59ea7e902b18f
SuMing777 <15010772645@163.com>
2020-08-23 01:24:23 +0800

    初始化

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: SuMing777
+ * @Date: 2020-08-22 13:12:40
+ * @LastEditors: SuMing777
+ * @LastEditTime: 2020-08-23 01:13:32
+ */
+import React from "react";
+import ReactDom from "react-dom";
+import "font-awesome/css/font-awesome.min.css";
+import "./index.css";
+import "./index.scss";
+
+ReactDom.render(
+  <div>
+    <i className="fa fa-address-book"></i>
+    <h1>Hello, world!</h1>
+  </div>,
+  document.getElementById("app")
+);
